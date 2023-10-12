3f3108cf45c0ef90f967285a32f794a34db3f9e6
zmfhshs <zmfhshs@gamil.com>
2023-10-12 06:37:30 +0000

    sdasdas

---
+++ chap4/prob3/line.c
@@ -0,0 +1,26 @@
+ #include <stdio.h>
+ #define MAXLINE 80
+ #include <stdlib.h>
+
+int main(int argc, char *argv[])
+ {
+     FILE *fp;
+     int line = 0;
+     char buffer[MAXLINE];
+
+if (argc != 2) {
+	fprintf(stderr,"How to use : line FileName\n");
+	exit(1);
+	}
+if ( (fp = fopen(argv[1],"r")) == NULL)
+   {
+	fprintf(stderr, "Error Open File\n"); 
+	exit(2);
+}
+	while (fgets(buffer, MAXLINE, fp) != NULL) { 
+	line++;
+	printf("%3d %s", line, buffer); 
+}
+	exit(0);
+}
+
---
+++ chap4/prob5/fscan.c
@@ -0,0 +1,23 @@
+#include <stdio.h> 
+
+struct student {
+   int id;
+   char name[20];
+   short score;
+};
+
+int main(int argc, char* argv[]) { 
+   struct student rec;
+   FILE *fp;
+   if (argc != 2) {
+      fprintf(stderr, "How to use: %s FileName\n", argv[0]);
+      return 1; 
+   }
+   fp = fopen(argv[1], "r");
+   printf("%-9s %-7s %-4s\n", "StudentID", "Name", "Score"); 
+   while (fscanf(fp,"%d %s %d", &rec.id, rec.name, &rec.score)==3) 
+      printf("%10d %6s %6d\n", rec.id, rec.name, rec.score);
+   fclose(fp);
+   return 0;
+} 
+
